bf3a3fdf2d22184fbb51b1dbceb4de567e60c9b7
hayley <hayley@drumlithie.com>
2015-12-10 10:28:26 +0000

    cw

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
    <!-- <script src="bootstrap.min.js"></script>-->
     <link rel="stylesheet" href="style.css">
-    <?PHP require 'db_connect.php';?>
+    
 </head>
 <body>
 
