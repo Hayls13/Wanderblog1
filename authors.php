65b6ff9db308d61c3018fe6a9ab4d9acc5f951dd
hayley <hayley@drumlithie.com>
2015-12-10 10:51:37 +0000

    cw

--- authors.php
+++ authors.php
@@ -1,11 +1,14 @@
 <!DOCTYPE HTML>
 <html lang="en">
 
+<link rel="stylesheet" href="style.css">
+
+
 <div id="bio">
+    <img src="https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=300&h=300">
     <h3>Name: </h3>
     <h3>Location: </h3>
     <h3>Description: </h3>
-    <img src="https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=300&h=300">
 
 </div>
 
