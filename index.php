b6f08d6d4c06417ca3c4f33cdb2ade70f5a8c73f
hayley <hayley@drumlithie.com>
2015-12-14 12:59:28 +0000

    cw

--- index.php
+++ index.php
@@ -46,9 +46,9 @@
             <h2>Register</h2>
             <hr>
             <label for="fname" class="ui-hidden-accessible">First name:</label>
-            <input id="fname" name="fname" placeholder="first name" type="text">
+            <input id="fname" name="fname" placeholder="first name" type="text"><br>
             <label for="lname" class="ui-hidden-accessible">Last name:</label>
-            <input id="lname" name="lname" placeholder="last name" type="text">
+            <input id="lname" name="lname" placeholder="last name" type="text"><br>
             <label for="email" class="ui-hidden-accessible">Email address:</label>
             <input id="email" name="email" placeholder="email" type="text"><br>
             <label for="pswd" class="ui-hidden-accessible">Password:</label>
