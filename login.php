52f3f04f4a7ee33f30daf0874f77d05e8f202d3b
hayley <hayley@drumlithie.com>
2015-12-14 14:49:23 +0000

    login

--- login.php
+++ login.php
@@ -1,1 +1,22 @@
 <?php
+
+$db = new mysqli(
+    "br-cdbr-azure-south-a.cloudapp.net",
+    "beff680d970100",
+    "29313567",
+    "hr1300777"
+);
+
+if($db->connect_errno){
+    die('Connectfailed['.$db->connect-error.']');
+}
+else echo "<p> Connection sucessful</p>";
+
+
+
+
+$email = $_POST["email"];
+$pword = $_POST["pword"];
+echo $email . $pword;
+
+?>
