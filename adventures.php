708f658b13482a1b02961be160296b1a0bae6191
hayley <hayley@drumlithie.com>
2015-12-14 17:17:29 +0000

    login

--- adventures.php
+++ adventures.php
@@ -1,5 +1,24 @@
 <!DOCTYPE HTML>
 <html lang="en">
 
+<title>Wanderblog</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script src="/bootstrap/js/bootstrap.min.js"></script>
+<script src="JS/actions.js"></script>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1> Adventures </h1>
+<ul>
+    <li>filter by highest vote</li>
+    <!--when click script an action that will sort from highest to lowest votes-->
+    <li><form action="adventures.php">
+            Search Adventures:
+            <input type="search" name="googlesearch">
+            <input type="submit">
+        </form>
+    </li>
+</ul>
 
 <?php
