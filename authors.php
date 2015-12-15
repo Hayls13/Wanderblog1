f624653abb4ab376db3026e4fcbcba66e1961afd
hayley <hayley@drumlithie.com>
2015-12-15 15:56:25 +0000

    login

--- authors.php
+++ authors.php
@@ -5,12 +5,14 @@
 
 <!--if login == successful
             load page-->
-
+<?php
+include('session.php');
+?>
 
 <div id="bio">
     <!--placeholder for profile image if used-->
     <img src="https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=300&h=300">
-    <h3>Name: </h3>
+    <h3>Name: </h3><i><?php echo $login_session; ?></i>
     <h3>Location: </h3>
     <h3>Description: </h3>
 
@@ -19,4 +21,4 @@
 <div id="adv">
 
 </div>
-<?php
+
--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+include ('session.php');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -16,32 +17,32 @@ if($db->connect_errno){
 else echo "<p> Connection sucessful</p>";
 
 
-$email = $_POST["email"];
+/*$email = $_POST["email"];
 $password = $_POST["password"];
-echo "you're logged in " . $email;
+echo "you're logged in " . $email;*/
 
 session_start(); // Starting Session
 $error=''; // Variable To Store Error Message
 if (isset($_POST['enter'])) {
-    if (empty($_POST['username']) || empty($_POST['password'])) {
-        $error = "Username or Password is invalid";
+    if (empty($_POST['email']) || empty($_POST['password'])) {
+        $error = "Email or Password is invalid";
     }
     else
     {
 // Define $username and $password
-        $username=$_POST['username'];
+        $email=$_POST['email'];
         $password=$_POST['password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-        $connection = mysql_connect("localhost", "root", "");
+        $connection = mysql_connect($address, $root, "");
 // To protect MySQL injection for Security purpose
-        $username = stripslashes($username);
+        $email = stripslashes($email);
         $password = stripslashes($password);
-        $username = mysqli_real_escape_string($username);
+        $email = mysqli_real_escape_string($email);
         $password = mysqli_real_escape_string($password);
 // Selecting Database
-        $db = mysql_select_db("company", $connection);
+        $db = mysqli_select_db('hr1300777', $connection);
 // SQL query to fetch information of registerd users and finds user match.
-        $query = mysqli_query("select * from logins where password='$password' AND username='$username'", $connection);
+        $query = mysqli_query($db,"select * from logins where password='$password' AND username='$username'", $connection);
         $rows = mysqli_num_rows($query);
         if ($rows == 1) {
             $_SESSION['login_user']=$username; // Initializing Session
