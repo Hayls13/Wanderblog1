af66d2dc63d6280b86eb8af5f00d3a07128a0019
hayley <hayley@drumlithie.com>
2015-12-15 14:11:17 +0000

    login

--- login.php
+++ login.php
@@ -20,15 +20,35 @@ $email = $_POST["email"];
 $pword = $_POST["pword"];
 echo "you're logged in " . $email;
 
-$sql_query = "SELECT * FROM logins WHERE email = '.$email.'";
-$result = $db->query($sql_query);
-print_r($result);
-
-while($row = $result->fetch_array()){
-    while($row = $result->fetch_array()){
-        echo "Hello user" . $row['email'];
+session_start(); // Starting Session
+$error=''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+    if (empty($_POST['username']) || empty($_POST['password'])) {
+        $error = "Username or Password is invalid";
+    }
+    else
+    {
+// Define $username and $password
+        $username=$_POST['username'];
+        $password=$_POST['password'];
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+        $connection = mysql_connect("localhost", "root", "");
+// To protect MySQL injection for Security purpose
+        $username = stripslashes($username);
+        $password = stripslashes($password);
+        $username = mysql_real_escape_string($username);
+        $password = mysql_real_escape_string($password);
+// Selecting Database
+        $db = mysql_select_db("company", $connection);
+// SQL query to fetch information of registerd users and finds user match.
+        $query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
+        $rows = mysql_num_rows($query);
+        if ($rows == 1) {
+            $_SESSION['login_user']=$username; // Initializing Session
+            header("location: profile.php"); // Redirecting To Other Page
+        } else {
+            $error = "Username or Password is invalid";
+        }
+        mysql_close($connection); // Closing Connection
     }
 }
-$result->close();
-    $db->close();
-?>
