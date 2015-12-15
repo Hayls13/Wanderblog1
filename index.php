581e6b9ec199d726f50276be1a487030e9e63b0f
hayley <hayley@drumlithie.com>
2015-12-15 17:53:02 +0000

    login

--- index.php
+++ index.php
@@ -66,59 +66,59 @@
     </div>
 
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-        <li data-target="#myCarousel" data-slide-to="3"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-        <div class="item active">
-            <img src="https://placeholdit.imgix.net/~text?txtsize=100&txt=1200%C3%97800&w=1200&h=800" alt="adv1">
-            <a href="#">Click here to view this adventure</a>
-            <a href="#">Click here to see more adventures</a>
-        </div>
-
-        <div class="item">
-            <img src="https://placeholdit.imgix.net/~text?txtsize=100&txt=1200%C3%97800&w=1200&h=800" alt="adv2">
-            <a href="#">Click here to view this adventure</a>
-            <a href="#">Click here to see more adventures</a>
-        </div>
-
-        <div class="item">
-            <img src="https://placeholdit.imgix.net/~text?txtsize=100&txt=1200%C3%97800&w=1200&h=800" alt="adv3">
-            <a href="#">Click here to view this adventure</a>
-            <a href="#">Click here to see more adventures</a>
-        </div>
-
-        <div class="item">
-            <img src="https://placeholdit.imgix.net/~text?txtsize=100&txt=1200%C3%97800&w=1200&h=800" alt="adv4">
-            <a href="#">Click here to view this adventure</a>
-            <a href="#">Click here to see more adventures</a>
-        </div>
-
-        <div class="item">
-            <img src="https://placeholdit.imgix.net/~text?txtsize=100&txt=1200%C3%97800&w=1200&h=800" alt="adv5">
-            <a href="#">Click here to view this adventure</a>
-            <a href="#">Click here to see more adventures</a>
-        </div>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-
-</div>
+<!--<div id="myCarousel" class="carousel slide" data-ride="carousel">-->
+<!--    <!-- Indicators -->-->
+<!--    <ol class="carousel-indicators">-->
+<!--        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>-->
+<!--        <li data-target="#myCarousel" data-slide-to="1"></li>-->
+<!--        <li data-target="#myCarousel" data-slide-to="2"></li>-->
+<!--        <li data-target="#myCarousel" data-slide-to="3"></li>-->
+<!--    </ol>-->
+<!---->
+<!--    <!-- Wrapper for slides -->-->
+<!--    <div class="carousel-inner" role="listbox">-->
+<!--        <div class="item active">-->
+<!--            <img src="https://placeholdit.imgix.net/~text?txtsize=100&txt=1200%C3%97800&w=1200&h=800" alt="adv1">-->
+<!--            <a href="#">Click here to view this adventure</a>-->
+<!--            <a href="#">Click here to see more adventures</a>-->
+<!--        </div>-->
+<!---->
+<!--        <div class="item">-->
+<!--            <img src="https://placeholdit.imgix.net/~text?txtsize=100&txt=1200%C3%97800&w=1200&h=800" alt="adv2">-->
+<!--            <a href="#">Click here to view this adventure</a>-->
+<!--            <a href="#">Click here to see more adventures</a>-->
+<!--        </div>-->
+<!---->
+<!--        <div class="item">-->
+<!--            <img src="https://placeholdit.imgix.net/~text?txtsize=100&txt=1200%C3%97800&w=1200&h=800" alt="adv3">-->
+<!--            <a href="#">Click here to view this adventure</a>-->
+<!--            <a href="#">Click here to see more adventures</a>-->
+<!--        </div>-->
+<!---->
+<!--        <div class="item">-->
+<!--            <img src="https://placeholdit.imgix.net/~text?txtsize=100&txt=1200%C3%97800&w=1200&h=800" alt="adv4">-->
+<!--            <a href="#">Click here to view this adventure</a>-->
+<!--            <a href="#">Click here to see more adventures</a>-->
+<!--        </div>-->
+<!---->
+<!--        <div class="item">-->
+<!--            <img src="https://placeholdit.imgix.net/~text?txtsize=100&txt=1200%C3%97800&w=1200&h=800" alt="adv5">-->
+<!--            <a href="#">Click here to view this adventure</a>-->
+<!--            <a href="#">Click here to see more adventures</a>-->
+<!--        </div>-->
+<!--    </div>-->
+<!---->
+<!--    <!-- Left and right controls -->-->
+<!--    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">-->
+<!--        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>-->
+<!--        <span class="sr-only">Previous</span>-->
+<!--    </a>-->
+<!--    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">-->
+<!--        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>-->
+<!--        <span class="sr-only">Next</span>-->
+<!--    </a>-->
+<!---->
+<!--</div>-->
 
 </body>
 </html>
