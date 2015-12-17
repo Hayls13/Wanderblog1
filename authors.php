1c952868c01821d7ac8cf335bd324a44d4dc8583
Hayls13 <hayley@drumlithie.com>
2015-12-17 18:24:11 +0000

    cw

--- authors.php
+++ authors.php
@@ -23,8 +23,9 @@ $locationr = $db->query($location);
     <!--placeholder for profile image if used-->
     <img src="https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=300&h=300">
     <h3>Name: </h3><?php echo "<h3>" . $login_session . "<h3>"; ?>
-    <h3>Location: </h3><?php echo $locationr; ?>
+    <h3>Location: </h3><?php echo $location; ?>
     <h3>Description: </h3>
+    <h3>Past Adventures: </h3>
     <h3><a href="logout.php">Logout</a></h3>
 
 </div>
