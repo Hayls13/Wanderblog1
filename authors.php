dbf6e6a09bb90b0c563567f166dc198ec606c98b
Hayls13 <hayley@drumlithie.com>
2015-12-17 18:50:20 +0000

    cw

--- authors.php
+++ authors.php
@@ -19,20 +19,33 @@ $location = "SELECT country FROM logins WHERE  email = '".$login_session."'";
 $locationr = $db->query($location);
 
 
+
+
 ?>
 
 <div id="bio">
     <!--placeholder for profile image if used-->
     <img src="https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=300&h=300">
+
     <h3>Name: </h3><?php echo "<h4>" . $login_session . "<h4>"; ?>
+
     <?php
     while( $result = $locationr->fetch_array()){
-        echo "<h3>Location:".$result['country']."</h3>";
+        echo "<h3>Location:". $result['country']."</h3>";
     }
     ?>
     <h3>Description: </h3>
+
     <h3>Past Adventures: </h3>
+
     <h3><a href="logout.php">Logout</a></h3>
+
+    <?php
+
+    $locationr->close();
+
+
+    ?>
 
 </div>
 
