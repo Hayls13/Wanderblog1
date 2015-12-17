0a14378732f993774b0dcb910a313e7077e974c9
Hayls13 <hayley@drumlithie.com>
2015-12-17 18:28:48 +0000

    cw

--- authors.php
+++ authors.php
@@ -23,7 +23,7 @@ $locationr = $db->query($location);
 <div id="bio">
     <!--placeholder for profile image if used-->
     <img src="https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=300&h=300">
-    <h3>Name: </h3><?php echo "<h3>" . $login_session . "<h3>"; ?>
+    <h3>Name: </h3><?php echo "<h2>" . $login_session . "<h2>"; ?>
     <h3>Location: </h3><?php echo $locationr; ?>
     <h3>Description: </h3>
     <h3>Past Adventures: </h3>
