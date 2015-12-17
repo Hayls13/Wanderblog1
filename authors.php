845065f60e9592a4ff1076c0c67e611129996eb1
Hayls13 <hayley@drumlithie.com>
2015-12-17 15:11:38 +0000

    cw

--- authors.php
+++ authors.php
@@ -5,8 +5,15 @@
 
 
 <?php
-$login_session = $_SESSION['login_user'];
+$db = new mysqli(
+    "br-cdbr-azure-south-a.cloudapp.net",
+    "beff680d970100",
+    "29313567",
+    "hr1300777"
+);
 
+$login_session = $_SESSION['login_user'];
+$location = $db->
 ?>
 
 <div id="bio">
