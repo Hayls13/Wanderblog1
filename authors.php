fb94475081124db5a28f258f103cbeb9ec0a05a7
Hayls13 <hayley@drumlithie.com>
2015-12-17 15:41:20 +0000

    cw

--- authors.php
+++ authors.php
@@ -14,13 +14,14 @@ $db = new mysqli(
 
 $login_session = $_SESSION['login_user'];
 
+$location = "SELECT location FROM logins WHERE  .  "'$login_session'" . = email";
 ?>
 
 <div id="bio">
     <!--placeholder for profile image if used-->
     <img src="https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=300&h=300">
     <h3>Name: </h3><?php echo "<h3>" . $login_session . "<h3>"; ?>
-    <h3>Location: </h3>
+    <h3>Location: </h3><?php echo $location; ?>
     <h3>Description: </h3>
     <h3><a href="logout.php">Logout</a></h3>
 
