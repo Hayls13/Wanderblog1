10e433a8aabad61e141b237e9cb39c9fc692c4c0
Hayls13 <hayley@drumlithie.com>
2015-12-17 01:40:16 +0000

    cw

--- adventures.php
+++ adventures.php
@@ -4,8 +4,8 @@
 <link rel="stylesheet" href="style.css">
 <title>Wanderblog</title>
 <head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<script src="/bootstrap/bootstrap/css/bootstrap.css"></script>
+<link rel="stylesheet" href="/bootstrap/bootstrap/css/bootstrap.min.css">
+<script src="/bootstrap/bootstrap/js/bootstrap.js"></script>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
