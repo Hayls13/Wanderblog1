e31d4a0564ca37cc96c3a84bc18753b92059a210
omattsa <matt.sandham@hotmail.co.uk>
2015-12-17 12:47:28 +0000

    testing

--- register.php
+++ register.php
@@ -1,13 +1,26 @@
 <?php
-/*
 
+/*
   $sql = "INSERT INTO register VALUES ('$firstname','$lastname', '$email', '$password', '$usertype');
  if ($result = $db->query($sql)){
-  echo "<p> Registration sent for verification</p>"
+  echo "<p> Registration sent for verification</p>";
 
 */
 
-echo "hello guys";
-
 ?>
+
+<h1>Register</h1>
+
+<form action="" method=""post>
+    <ul>
+        <li>
+            Username*:<br>
+            <input type="text" name="username">
+        </li>
+
+        <li></li>
+    </ul>
+
+</form>
+
 
