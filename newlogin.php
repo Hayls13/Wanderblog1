0fce7efa39c1259b2e80cc6bede9f472be3b6bba
Hayls13 <hayley@drumlithie.com>
2016-05-18 23:37:09 +0100

    cw

---
+++ newlogin.php
@@ -0,0 +1,68 @@
+<?php
+?>
+
+<HMTML>
+
+    <div id="container">
+        <a class="hiddenanchor" id="toregister"></a>
+        <a class="hiddenanchor" id="tologin"></a>
+    <div id="wrapper">
+        <div id="login" class="animate form">
+            <form action="..." autocomplete="on">
+                  <h1>Log In</h1>
+                  <p>
+                      <label for="email" class="email" data-icon="u" > Your email </label>
+                      <input id="email" name="email" required="required" type="tezt" placeholder="your email" />
+                  </p>
+                  <p>
+                      <label for="password" class="pword" data-icon="p">Your Password </label>
+                      <input id="password" name="password" required="required" type="password" placeholder="..." />
+                  </p>
+                  <p class="login button">
+                      <input type="submit" value="Login" />
+                  </p>
+                  <p class="change_link">
+                      Not a member yet?
+                      <a href="#toregister" class="to_register">Join Us</a>
+                  </p>
+                </form>
+        </div>
+
+        <div id="register" class="animate form">
+            <form action="..." autocomplete="on">
+                <h1> Sign Up </h1>
+                <p>
+                    <label for="emailS" class="email" data-icon="u">Your Email</label>
+                    <input id="emailS" name="emailS" required="required" type="text" placeholder="email" />
+                </p>
+                
+            </form>
+        </div>
+    </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</HMTML>
