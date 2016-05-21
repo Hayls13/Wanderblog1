453bb3499a9dfd62f2edca1fd1124637a92116ff
Hayls13 <hayley@drumlithie.com>
2016-05-21 23:14:13 +0100

    cw

--- newlogin.php
+++ newlogin.php
@@ -8,6 +8,7 @@
 <p id="onclick"> Popup </p>
 </div>
 
+<!--onclick popup-->
 <div id="logindiv">
     <form class="form" action="#" id="login">
         <h3> Log In </h3>
@@ -21,6 +22,31 @@
             <br/>
         <input type="button" id="send" value="send"/>
         <input type="button" id="cancel" value="cancel"/>
+    </form>
+</div>
+
+<!--register-->
+<div id="register">
+    <form class="form" action="#" id="register">
+        <h3>Register</h3>
+        <hr/><br/>
+        <label>First Name: <span>*</span></label><br/>
+            <input type="text" id="Fname" placeholder="First Name"/><br/>
+        <br/>
+        <label>Last Name: <span>*</span></label><br/>
+            <input type="text" id="Lname" placeholder="Last Name"/><br/>
+        <br/>
+        <label>Email: <span>*</span></label><br/>
+            <input type="text" id="email" placeholder="email"/><br/>
+        <br/>
+        <label>Password: <span>*</span></label>
+        <br/>
+            <input type="text" id="password" placeholder="password"/><br/>
+        <br/>
+        <input type="button" id="registerbtn" value="Register"/>
+        <input type="button" id="cancel" value="Cancel"/>
+        <br/>
+
     </form>
 </div>
 
