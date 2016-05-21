6e3bbfedb0fcdcb0fd8053c46d6e1c2116fafd86
Hayls13 <hayley@drumlithie.com>
2016-05-21 22:56:39 +0100

    cw

--- newlogin.php
+++ newlogin.php
@@ -4,75 +4,25 @@
 <?php
 ?>
 
+<div class="form" id="popup">
+<p id="onclick"> Popup </p>
+</div>
 
-
-<head>
-    <title>Wanderblog</title>
-        <link rel="stylesheet"  href="login.css" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-</head>
-
-<body>
-    <div id="container">
-        <a class="hiddenanchor" id="toregister"></a>
-        <a class="hiddenanchor" id="tologin"></a>
-    <div id="wrapper">
-        <div id="login" class="animate form">
-            <form action="#" autocomplete="on">
-                  <h1>Log In</h1>
-                  <p>
-                      <label for="email" class="email" > Your email </label>
-                      <input id="email" name="email" required="required" type="text" placeholder="your email" />
-                  </p>
-                  <p>
-                      <label for="password" class="pword" >Your Password </label>
-                      <input id="password" name="password" required="required" type="password" placeholder="..." />
-                  </p>
-                  <p class="login button">
-                      <input type="submit" value="Login" />
-                  </p>
-                  <p class="change_link">
-                      Not a member yet?
-                      <a href="#register" class="register">Join Us</a>
-                  </p>
-                </form>
-        </div>
-
-        <div id="register" class="animate form">
-            <form action="#" autocomplete="on">
-                <h1> Sign Up </h1>
-                <p>
-                    <label for="emailS" class="email" >Your Email</label>
-                    <input id="emailS" name="emailS" required="required" type="text" placeholder=" Email" />
-                </p>
-                <p>
-                    <label for="FName" class="Fname" >First Name</label>
-                    <input id="Fname" name="Fname" required="required" type="text" placeholder=" First Name" />
-                </p>
-                <p>
-                    <label for="LName" class="Lname" >Last Name</label>
-                    <input id="Lname" name="Lname" required="required" type="text" placeholder=" Last Name" />
-                </p>
-                <p>
-                    <label for="PwordS" class="PwodS">Password </label>
-                    <input id="PwordS" name="PwordS" required="required" type="text" placeholder=" Password" />
-                </p>
-                <p class="signin button">
-                    <input type="submit" value="Sign Up"/>
-                </p>
-
-                <p class="change_link">
-                    Already a member?
-                    <a href="#tologin" class="to_register"> Go and log in </a>
-                </p>
-            </form>
-        </div>
-
-    </div>
-    </div>
-</body>
-
-
+<div id="logindiv">
+    <form class="form" action="#" id="login">
+        <h3> Log In </h3>
+            <hr/><br/>
+                <label>Name: <span>*</span></label>
+            <br/>
+        <input type="text" id="name" placeholder="name"><br/>
+            <label>Email: <span>*</span></label>
+            <br/>
+        <input type="text" id="email" placeholder="email"><br/>
+            <br/>
+        <input type="button" id="send" value="send"/>
+        <input type="button" id="cancel" value="cancel"/>
+    </form>
+</div>
 
 
 
