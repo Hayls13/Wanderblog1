51e87d177bed1759ad201b4338c754a46b6d653f
Hayls13 <hayley@drumlithie.com>
2016-05-21 23:37:56 +0100

    cw

--- popup.js
+++ popup.js
@@ -5,12 +5,12 @@
 function popup() {
     $("#logindiv").css("display", "block");
 }
-$("#login #cancel").click(function() {
+$("#send #cancel").click(function() {
     $(this).parent().parent().hide();
 });
 $("#onclick").click(function() {
     $("#contactdiv").css("display", "block");
 });
-$("#contact #cancel").click(function() {
+$("#register #cancel").click(function() {
     $(this).parent().parent().hide();
 });
