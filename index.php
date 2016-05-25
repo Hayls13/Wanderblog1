bca3d6acf27e2440d8836f56d7b622c704d2e722
Hayls13 <hayley@drumlithie.com>
2016-05-25 23:50:06 +0100

    cw

--- index.php
+++ index.php
@@ -27,10 +27,10 @@
 
     <!--top menu-->
 <ul id="mymenu">
+    <li><a href="adventures.php">Search</a></li>
     <li><form action="register.php" method="post" name="register">
             <button id="registerbtn">Register</button>
         </form></li>
-    <li><a href="adventures.php">Search</a></li>
     <li><form action="login.php" id="form" method="post" name="login">
         <input id="email" name="email" placeholder="email" type="text">
         <input id="password" name="password" placeholder="password" type="password">
