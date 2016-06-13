32777973182a955b8e8d76f2196f932a94b52c42
Hayls13 <hayley@drumlithie.com>
2016-06-13 02:38:01 +0100

    cw

--- adventureinfo.php
+++ adventureinfo.php
@@ -1,9 +1,22 @@
 <?php
 
+include ("adventures.php");
+
+$db = new mysqli(
+    "br-cdbr-azure-south-a.cloudapp.net",
+    "beff680d970100",
+    "29313567",
+    "hr1300777"
+);
+
+
+
 $title = $row['title'];
 
 
 echo "test";
 
 echo "<p>" . $title . "</p>";
+
+?>
 
